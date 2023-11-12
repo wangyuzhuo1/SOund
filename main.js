2a4d7df2ad26bc49c5955e0c332b51db059ab2e6
wangyuzhuo1 <113219030+wangyuzhuo1@users.noreply.github.com>
2023-11-11 20:54:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+//https://teachablemachine.withgoogle.com/models/sOZ1WmvC1/
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true})
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/sOZ1WmvC1/.model.json", modelready)
+}
+function modelready(){
+    classifier.classify(got_results)
+}
+function got_results(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        randomnumbers_r=Math.floor(Math.random()*255)+1;
+        randomnumbers_g=Math.floor(Math.random()*255)+1;
+        randomnumbers_b=Math.floor(Math.random()*255)+1;
+        
+        document.getElementById("result_label").innerHTML='I can hear - '+results[0].label;
+        document.getElementById("result_confidence").innerHTML='Accuracy - '(results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color="rgb("+randomnumbers_r+", "+randomnumbers_g+", "+randomnumbers_b+")";
+        document.getElementById("result_confidence").style.color="rgb("+randomnumbers_r+", "+randomnumbers_g+", "+randomnumbers_b+")";
+
+        img1=document.getElementById("alien1");
+        img2=document.getElementById("alien2");
+        img3=document.getElementById("alien3");
+        img4=document.getElementById("alien4");
+
+        if(results[0].label=="Clapping"){
+            img1.src="aliens-01.gif";
+            img2.src="aliens-02.png";
+            img3.src="aliens-03.png";
+            img4.src="aliens-04.png";
+        }
+        else if(results[0].label=="Tapping"){
+            img1.src="aliens-01.png";
+            img2.src="aliens-02.gif";
+            img3.src="aliens-03.png";
+            img4.src="aliens-04.png";
+        }
+        else if(results[0].label=="Snapping"){
+            img1.src="aliens-01.png";
+            img2.src="aliens-02.png";
+            img3.src="aliens-03.gif";
+            img4.src="aliens-04.png";
+        }
+        else{
+            img1.src="aliens-01.png";
+            img2.src="aliens-02.png";
+            img3.src="aliens-03.png";
+            img4.src="aliens-04.gif";
+        }
+    }
+}
